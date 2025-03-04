2c43a9770350a26d94352812425d7908ca01c18f
Sandeep <sndpgauti@gmail.com>
2025-03-05 00:31:26 +0530

    data transformation completed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from networksecurity.entity.config_entity import DataIngestionConfig, DataValida
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformationArtifact, DataTransformation, DataTransformationConfig
 import sys
 
 if __name__=='__main__':
@@ -24,7 +25,11 @@ if __name__=='__main__':
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
         print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        data_transformation=DataTransformation(data_validation_artifact, data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
         
+        print(data_transformation_artifact)
         
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -31,5 +31,20 @@ DATA_VALIDATION_VALID_DIR: str = "validated"
 DATA_VALIDATION_INVALID_DIR: str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME: str="preprocessor.pkl"
+
+# Data Transformation related constant start with Data_Transformation Var NAME
+
+DATA_TRANSFORMATION_DIR_NAME="data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR="transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR="transformed_object"
+
+# KNN imputer class to replace nan values inplace of missing values
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict={
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform",
+    
+}
 
 
